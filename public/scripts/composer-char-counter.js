f3781b468a0040dc756babc20c827f005d189a62
Krisan Manoharan <krisan1010@hotmail.com>
2024-05-24 08:48:24 -0400

    Add conditional to change color after counter goes below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,16 @@ $(document).ready(function() {
     // Calculate the number of characters left
     var textLength = $(this).val().length;
     var charactersLeft = 140 - textLength;
-
-    console.log('Characters left:', charactersLeft);
+    
+    // Update the counter in the HTML
+    var counterElement = $(this).siblings('div').find('.counter');
+    counterElement.text(charactersLeft);
+    
+    // Change the counter color and prevent further input if limit is reached
+    if (charactersLeft < 0) {
+      counterElement.css('color', 'red');
+    } else {
+      counterElement.css('color', '');
+    }
   });
 });
