56be59fc27ea833652607358d1dee3a08e2c0b7b
Krisan Manoharan <krisan1010@hotmail.com>
2024-05-24 09:42:28 -0400

    Add createNewTweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,25 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+const createNewTweet = function(tweet) {
+  
+  let $tweet = `<article>
+    <header class="tweet">
+      <img src=${tweet.user.avatars} >
+        <div>
+          <h4 class="name">${tweet.user.name}</h4>
+          <h4 class="handle">${tweet.user.handle}</h4>
+        </div>
+    </header>
+    <p>${escape(tweet.content.text)}</p>
+    <footer>
+      <p>${timeago.format(tweet["created_at"])}</p>
+      <div>
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-regular fa-heart"></i>
+      </div>
+    </footer>
+  </article>`;
+  return $tweet;
+};
