0f82db433d1c1cf420221a245d0b580df5a3647e
Krisan Manoharan <krisan1010@hotmail.com>
2024-05-24 08:36:58 -0400

    Add logic

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+  $('.new-tweet textarea').on('input', function() {
+    // Calculate the number of characters left
+    var textLength = $(this).val().length;
+    var charactersLeft = 140 - textLength;
 
+    console.log('Characters left:', charactersLeft);
+  });
 });
