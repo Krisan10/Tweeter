0fba1d598ef2d933e687eb754764ed292969a552
Krisan Manoharan <krisan1010@hotmail.com>
2024-05-27 21:19:48 -0400

    Fix functions

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('.new-tweet textarea').on('input', function() {
     // Calculate the number of characters left
-    var textLength = $(this).val().length;
-    var charactersLeft = 140 - textLength;
+    let textLength = $(this).val().length;
+    let charactersLeft = 140 - textLength;
     
     // Update the counter in the HTML
-    var counterElement = $(this).siblings('div').find('.counter');
+    let counterElement = $(this).siblings('div').find('.counter');
     counterElement.text(charactersLeft);
     
     // Change the counter color and prevent further input if limit is reached
